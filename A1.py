f3e5d263a67dfa0886cc40a336b0537cf16cb758
jrbartosch <jbartosch+jed@gmail.com>
2025-02-03 08:17:08 +0600

    Add files via upload

---
+++ A1.py
@@ -0,0 +1,7 @@
+word = input('Please Enter a Word: ')
+for i in word:
+    if (i == 'a'):
+        print ('A is in this word.')
+        break
+    else:
+        print ('A has not been found yet in this word.')
